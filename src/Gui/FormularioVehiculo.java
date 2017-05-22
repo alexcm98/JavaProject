6cb3d632f71b2eddff0ad3eea4f30da8c71d12dc
Manu <Manu@DESKTOP-B2SPCM8>
2017-05-23 00:57:07 +0200

    Errores corregidos,cambios en la interfaz,keyevents añadidos

---
+++ src/Gui/FormularioVehiculo.java
@@ -0,0 +1,166 @@
+package Gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Toolkit;
+
+import javax.swing.JSlider;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class FormularioVehiculo {
+
+	protected JFrame frame;
+	private JTextField textMarca;
+	private JTextField textMatricula;
+	private JTextField textColor;
+	private JTextField textFecha;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FormularioVehiculo window = new FormularioVehiculo();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FormularioVehiculo() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(FormularioVehiculo.class.getResource("/Images/icon.png")));
+		frame.getContentPane().setBackground(Color.DARK_GRAY);
+		frame.setBounds(100, 100, 633, 573);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+		
+		JLabel lblVehculo = new JLabel("Registro de veh\u00EDculos");
+		lblVehculo.setForeground(Color.WHITE);
+		lblVehculo.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblVehculo.setBounds(45, 26, 319, 29);
+		frame.getContentPane().add(lblVehculo);
+		
+		JLabel lblMarca = new JLabel("Marca:");		
+		lblMarca.setForeground(Color.WHITE);
+		lblMarca.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblMarca.setBounds(62, 83, 52, 14);
+		frame.getContentPane().add(lblMarca);
+		
+		JLabel lblColor = new JLabel("Color:");
+		lblColor.setForeground(Color.WHITE);
+		lblColor.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblColor.setBounds(62, 195, 46, 14);
+		frame.getContentPane().add(lblColor);
+		
+		JLabel lblMatricula = new JLabel("Matricula:");
+		lblMatricula.setForeground(Color.WHITE);
+		lblMatricula.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblMatricula.setBounds(45, 135, 69, 14);
+		frame.getContentPane().add(lblMatricula);
+		
+		JLabel lblFecha = new JLabel("Fecha:");
+		lblFecha.setForeground(Color.WHITE);
+		lblFecha.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblFecha.setBounds(56, 247, 52, 14);
+		frame.getContentPane().add(lblFecha);
+		
+		JLabel lblPotenciaMotor = new JLabel("Potencia max:");
+		lblPotenciaMotor.setForeground(Color.WHITE);
+		lblPotenciaMotor.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblPotenciaMotor.setBounds(10, 312, 104, 14);
+		frame.getContentPane().add(lblPotenciaMotor);
+		
+		JLabel lblPotencia = new JLabel("Potencia = 0");
+		lblPotencia.setForeground(Color.WHITE);
+		lblPotencia.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblPotencia.setBounds(140, 337, 192, 42);
+		frame.getContentPane().add(lblPotencia);
+		
+		JSlider slider = new JSlider();
+		slider.setBackground(Color.DARK_GRAY);
+		slider.setForeground(Color.WHITE);
+		slider.setValue(0);
+		slider.setMaximum(300);
+		slider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				lblPotencia.setText("Potencia = " + slider.getValue() + " km/h");
+				if (slider.getValue() <= 100) {
+					slider.setBackground(Color.GREEN);
+				} else if (slider.getValue() <= 200) {
+					slider.setBackground(Color.orange);
+				} else if (slider.getValue() <= 300) {
+					slider.setBackground(Color.red);
+				}
+			}
+		});
+		slider.setBounds(132, 303, 200, 23);
+		frame.getContentPane().add(slider);
+		textMarca = new JTextField();
+		textMarca.setBounds(140, 82, 114, 20);
+		frame.getContentPane().add(textMarca);
+		textMarca.setColumns(10);
+		textMatricula = new JTextField();
+		textMatricula.setColumns(10);
+		textMatricula.setBounds(140, 134, 114, 20);
+		frame.getContentPane().add(textMatricula);
+		textColor = new JTextField();
+		textColor.setColumns(10);
+		textColor.setBounds(140, 194, 114, 20);
+		frame.getContentPane().add(textColor);
+		textFecha = new JTextField();
+		textFecha.setColumns(10);
+		textFecha.setBounds(140, 246, 114, 20);
+		frame.getContentPane().add(textFecha);
+		JButton btnCrear = new JButton("Crear");
+		btnCrear.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		btnCrear.setBounds(20, 441, 132, 56);
+		frame.getContentPane().add(btnCrear);
+		JButton btnEditar = new JButton("Editar");
+		btnEditar.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		btnEditar.setBounds(166, 441, 132, 56);
+		frame.getContentPane().add(btnEditar);
+		JButton btnCliente = new JButton("Cliente");
+		btnCliente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnCliente.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		btnCliente.setBounds(316, 441, 132, 56);
+		frame.getContentPane().add(btnCliente);
+		JButton btnReparar = new JButton("Reparar");
+		btnReparar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnReparar.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		btnReparar.setBounds(464, 441, 132, 56);
+		frame.getContentPane().add(btnReparar);
+	}
+}
---
+++ src/Gui/PaginaPrincipal.java
@@ -0,0 +1,188 @@
+package Gui;
+
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Event;
+
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.event.KeyAdapter;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+public class PaginaPrincipal {
+
+	protected JFrame frame;
+	private JButton btnBv;
+	private JButton btnNv;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PaginaPrincipal window = new PaginaPrincipal();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PaginaPrincipal() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.requestFocus();
+		frame.addKeyListener(new KeyListener() {			
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if(e.getKeyCode()==KeyEvent.VK_RIGHT)
+				{
+					btnNv.requestFocus();
+				}
+				if(e.getKeyCode()==KeyEvent.VK_LEFT)
+				{
+					btnBv.requestFocus();
+				}
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+			}
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				
+			}
+		});
+		frame.setFocusable(true);
+		frame.requestFocus();
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(PaginaPrincipal.class.getResource("/Images/icon.png")));
+		frame.getContentPane().setBackground(Color.DARK_GRAY);
+		frame.setTitle("P\u00E1gina Principal");
+		frame.setBounds(100, 100, 317, 205);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height / 2 - frame.getSize().height / 2);
+		
+		
+		btnBv = new JButton("Buscar Veh\u00EDculo");
+		btnBv.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+
+				btnBv.setIcon(new ImageIcon(PaginaPrincipal.class.getResource("/Images/lupa1.png")));
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				btnBv.setIcon(new ImageIcon(PaginaPrincipal.class.getResource("/Images/lupa3.png")));
+			}
+		});
+		btnBv.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if(e.getKeyCode()==KeyEvent.VK_RIGHT)
+				{
+					btnNv.requestFocus();
+				}
+				if(e.getKeyCode()==KeyEvent.VK_ENTER)
+				{
+					btnBv.doClick();
+				}
+			}
+		});
+		btnBv.setBounds(37, 27, 97, 106);
+		btnBv.setForeground(Color.WHITE);
+		frame.getContentPane().setLayout(null);
+		btnBv.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnBv.setRolloverIcon(new ImageIcon(PaginaPrincipal.class.getResource("/Images/lupa1.png")));
+		btnBv.setPressedIcon(new ImageIcon(PaginaPrincipal.class.getResource("/Images/lupa3.png")));
+		btnBv.setIcon(new ImageIcon(PaginaPrincipal.class.getResource("/Images/lupa2.png")));
+		btnBv.setVerticalTextPosition(SwingConstants.BOTTOM);
+		btnBv.setVerticalAlignment(SwingConstants.BOTTOM);
+		btnBv.setIconTextGap(-3);
+		btnBv.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnBv.setContentAreaFilled(false);
+		btnBv.setBorderPainted(false);
+		btnBv.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		frame.getContentPane().add(btnBv);
+
+		btnNv = new JButton("Nuevo Veh\u00EDculo");
+		btnNv.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				btnNv.setIcon(new ImageIcon(PaginaPrincipal.class.getResource("/Images/vehiculo1.png")));
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+
+				btnNv.setIcon(new ImageIcon(PaginaPrincipal.class.getResource("/Images/vehiculo3.png")));
+			}
+		});
+		btnNv.setVerticalTextPosition(SwingConstants.BOTTOM);
+		btnNv.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if(e.getKeyCode()==KeyEvent.VK_LEFT)
+				{
+					btnBv.requestFocus();
+				}
+				if(e.getKeyCode()==KeyEvent.VK_ENTER)
+				{
+					btnNv.doClick();
+				}
+			}
+		});
+		btnNv.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FormularioVehiculo formularioV = new FormularioVehiculo();
+				formularioV.frame.setVisible(true);
+				frame.dispose();
+			}
+		});
+		btnNv.setBounds(176, 15, 97, 118);
+		btnNv.setForeground(Color.WHITE);
+		btnNv.setRolloverIcon(new ImageIcon(PaginaPrincipal.class.getResource("/Images/vehiculo1.png")));
+		btnNv.setPressedIcon(new ImageIcon(PaginaPrincipal.class.getResource("/Images/vehiculo3.png")));
+		btnNv.setIcon(new ImageIcon(PaginaPrincipal.class.getResource("/Images/vehiculo2.png")));
+		btnNv.setVerticalAlignment(SwingConstants.BOTTOM);
+		btnNv.setIconTextGap(-3);
+		btnNv.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnNv.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNv.setContentAreaFilled(false);
+		btnNv.setBorderPainted(false);
+		btnNv.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		frame.getContentPane().add(btnNv);
+		btnBv.setFocusPainted(false);
+		btnNv.setFocusPainted(false);
+	}
+
+}
